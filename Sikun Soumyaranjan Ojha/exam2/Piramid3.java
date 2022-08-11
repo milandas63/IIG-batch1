cf3fcc410320398f80f4a2ce3ac1cb079db2d4f3
Sikunsoumyaranjan <110038520+Sikunsoumyaranjan@users.noreply.github.com>
2022-08-11 16:10:12 +0530

    Update Piramid3.java
    Signed-off-by: Sikunsoumyaranjan <110038520+Sikunsoumyaranjan@users.noreply.github.com>

--- Sikun Soumyaranjan Ojha/exam2/Piramid3.java
+++ Sikun Soumyaranjan Ojha/exam2/Piramid3.java
@@ -1,20 +1,21 @@
-public class Piramid3 {
-
-    public static void main(String[] args)
-    {
-
-        for (int i = 0; i <= 5; i++)
-        {
-            int alphabet = 65;
-            for (int j = 5; j > i; j--)
-            {
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= i; k++)
-            {
-                System.out.print((char) (alphabet + k) + " ");
-            }
-            System.out.println();
-        }
-    }
+import java.util.Scanner;
+class PiramidAlphabet{
+public static void main(String args[]){
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the number of rows");
+int rows=scan.nextInt();
+int i,j;
+for(i=1; i<=rows; i++){
+   for(j=1; j<=rows-i; j++){
+   System.out.print(" ");
+}
+   for(j=1; j<=i; j++){
+   System.out.print((char)(j+64));
+}
+    for(j=i-1; j>=1; j--){
+   System.out.print((char)(j+64));
+}
+System.out.println();
+}
+}
 }
