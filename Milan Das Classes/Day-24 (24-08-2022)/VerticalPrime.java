2f1be4a3cafff6100330fee9590f71edaacf6bf2
Milan Das <45796126+milandas63@users.noreply.github.com>
2022-08-24 16:11:17 +0530

    Add files via upload

---
+++ Milan Das Classes/Day-24 (24-08-2022)/VerticalPrime.java
@@ -0,0 +1,50 @@
+
+public class VerticalPrime {
+	public static int start = 2000;
+	public static int end = 5000;
+	public static int[][] output;
+	public static int maxRow = 25;
+	public static int maxCol = 15;
+
+	public static void main(String[] args) {
+		output = new int[maxRow][maxCol];
+		generate();
+		print();
+	}
+
+	private static void generate() {
+		boolean yesno;
+		int row = 0, col = 0;
+
+		for(int i=start; i<=end; i++) {
+			yesno = true;
+			for(int j=2; j<(i/2); j++) {
+				if(i%j==0) {
+					yesno = false;
+					break;
+				}
+			}
+			if(yesno) {
+				output[row][col] = i;
+				row++;
+				if(row>=maxRow) {
+					row = 0;
+					col++;
+					if(col>=maxCol) {
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+	private static void print() {
+		for(int row=0; row<output.length; row++) {
+			System.out.println();
+			for(int col=0; col<output[row].length; col++) {
+				if(output[row][col]>0)
+					System.out.print(output[row][col] + " ");
+			}
+		}
+	}
+}
