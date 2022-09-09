35bd771748c7ae162c6c92da16ea1d23f1383bff
Sikunsoumyaranjan <110038520+Sikunsoumyaranjan@users.noreply.github.com>
2022-09-09 14:41:02 +0530

    Add files via upload

---
+++ Sikun Soumyaranjan Ojha/Exam 6/Copyfile.java
@@ -0,0 +1,46 @@
+package Exam6;
+
+import java.util.Scanner;
+
+public class Copyfile {
+
+	public static void main(String[] args) {
+		int num;
+		boolean more = true;
+		Scanner s = new Scanner(System.in);
+		String   Name, Gender,Nationality, Religion,Location;
+		StringBuffer buf;
+		
+		while(more) {
+			buf = new StringBuffer();
+			
+			System.out.print("Name:");
+			Name = s.nextLine();
+			buf.append(Name+", ");
+			
+			System.out.print("Gender:");
+			Gender = s.nextLine();
+			buf.append(Gender+"\r\n");
+
+			
+			
+			System.out.print("Nationality:");
+			Nationality = s.nextLine();
+			buf.append(Nationality+", ");
+	
+			System.out.print("Religion:");
+			Religion = s.nextLine();
+			buf.append(Religion+", ");
+			
+			System.out.print("Location:");
+			Location = s.nextLine();
+			buf.append(Location+", ");
+	
+		    System.out.println();
+			System.out.print("more-y/n");
+			more = s.nextLine().toUpperCase().startsWith("Y");
+
+		}
+	}
+
+}
