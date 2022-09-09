7dd31baefacaca8463b38d5a8e61aa7c81741152
sipu17 <110605226+sipu17@users.noreply.github.com>
2022-09-09 14:47:57 +0530

    Add files via upload

---
+++ Asutosh Parida/exam6/no2.java
@@ -0,0 +1,48 @@
+package section2;
+
+import java.util.Scanner;
+
+public class no2 {
+
+	public static void main(String[] args) {
+		int num;
+		boolean more = true;
+		Scanner s = new Scanner(System.in);
+		String   Name, Gender,Nationality, Religion,Location;
+		StringBuffer buf;
+		
+		while(more) {
+			buf = new StringBuffer();
+			
+			System.out.print("Name:");
+			Name = s.nextLine();
+			buf.append(Name+", ");
+			
+			System.out.print("Gender:");
+			Gender = s.nextLine();
+			buf.append(Gender+"\r\n");
+
+			
+			
+			System.out.print("Nationality:");
+			Nationality = s.nextLine();
+			buf.append(Nationality+", ");
+	
+			System.out.print("Religion:");
+			Religion = s.nextLine();
+			buf.append(Religion+", ");
+			
+			System.out.print("Location:");
+			Location = s.nextLine();
+			buf.append(Location+", ");
+	
+		    System.out.println();
+			System.out.print("more-y/n");
+			more = s.nextLine().toUpperCase().startsWith("Y");
+
+
+
+	}
+
+	}
+}
