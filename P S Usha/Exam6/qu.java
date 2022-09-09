731755a15f6805a7b4d1c8a017080cb63f1992bd
Pratapaditya Mishra <110086722+Mishra-Adi175@users.noreply.github.com>
2022-09-09 14:29:44 +0530

    Add files via upload

---
+++ P S Usha/Exam6/qu.java
@@ -0,0 +1,22 @@
+package exam;
+
+public class qu {
+
+	public static void main(String[] args) {
+		int rows = 9;
+		for (int i = 1; i <= rows; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+
+			for (int j = i * 2; j < rows * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int l = i; l >= 1; l--) {
+				System.out.print(l);
+			}
+			System.out.println();
+		}
+
+	}
+}
