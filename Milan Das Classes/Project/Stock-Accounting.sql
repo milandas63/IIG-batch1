47ffd1c83090f3d2559e0b51658a1fc56755b980
Milan Das <45796126+milandas63@users.noreply.github.com>
2022-12-05 16:19:00 +0530

    Add files via upload
    Signed-off-by: Milan Das <45796126+milandas63@users.noreply.github.com>

--- Milan Das Classes/Project/Stock-Accounting.sql
+++ Milan Das Classes/Project/Stock-Accounting.sql
@@ -37,23 +37,66 @@ CREATE TABLE user(
     del                		ENUM("*")           DEFAULT NULL,
     PRIMARY KEY(user_id)
 );
-INSERT INTO user VALUES (1, 'Administrator', NULL, NULL);
+INSERT INTO user VALUES
+	(1, 'Administrator', NULL, NULL),
+	(2, 'Operator',      NULL, NULL);
 
-#############################
-###                       ###
-###    TABLE trn_header   ###
-###                       ###
-#############################
+
+#########################
+###                   ###
+###    TABLE config   ###
+###                   ###
+#########################
+SELECT "config";
+DROP TABLE IF EXISTS config;
+CREATE TABLE config(
+	id						INT(6)							NOT NULL AUTO_INCREMENT,
+	company_name            VARCHAR(100)					NOT NULL,
+	company_usp             VARCHAR(100),
+	company_address			VARCHAR(200),
+	gst_no					VARCHAR(100),
+	dl_no					VARCHAR(100),
+	user_id					INT(6)							NOT NULL REFERENCES user(user_id),
+	last_update				TIMESTAMP,
+    del                		ENUM("*")						DEFAULT NULL,
+    PRIMARY KEY(party_id)
+);
+INSERT INTO config VALUES
+	(1, 'I-Hub Pvt Ltd', 'Health is Wealth', NULL, NULL, NULL, 1, NULL, NULL);
+
+
+########################
+###                  ###
+###    TABLE party   ###
+###                  ###
+########################
 SELECT "party";
 DROP TABLE IF EXISTS party;
 CREATE TABLE party(
-	party_id				INT(6)				NOT NULL AUTO_INCREMENT,
-	party_name				VARCHAR(100)		NOT NULL,
-	gst_no					VARCHAR(20),
-	dl_no					VARCHAR(20),
-	
-
+	party_id				INT(6)							NOT NULL AUTO_INCREMENT,
+	party_name				VARCHAR(100)					NOT NULL,
+	party_type              ENUM('Buyer','Supplier')		NOT NULL,
+	address                 VARCHAR(100),
+	shiping_address         VARCHAR(100),
+	gst_no					VARCHAR(20)                     NOT NULL,
+	dl_no					VARCHAR(20)                     NOT NULL,
+	mobile_no               VARCHAR(20),
+	land_phone_no           VARCHAR(20),
+	user_id					INT(6)							NOT NULL REFERENCES user(user_id),
+	last_update				TIMESTAMP,
+    del                		ENUM("*")						DEFAULT NULL,
+    PRIMARY KEY(party_id)
 );
+INSERT INTO party VALUES
+	# Supplier
+	(1, 'Jagabandhu Medical',		'Supplier',	'PLOT NO.B-5, Sahid Nagar, Bhubaneshwar 751007',					 NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	(2, 'Jayant Pharmaceuticals',	'Supplier', 'Plot No.- 40 EAST, Janpath Road, Ashok Nagar, Bhubaneshwar 751009', NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	(3, 'Bajoria Enterprisers',		'Supplier', ' Jaunliapatti, Cuttack 753009',									 NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	# Buyer
+	(1001, 'Maa Tarini Medical',		'Buyer', 'Bhubaneswar',	NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	(1002, 'Sobhagya Drug House',		'Buyer', 'Bhubaneswar',	NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	(1003, 'Medicare Pharmaceuticals',	'Buyer', 'Bhubaneswar',	NULL, NULL, NULL, '', '', 1, NULL, NULL),
+	(1004, 'Panda Medicine Store',		'Buyer', 'Bhubaneswar',	NULL, NULL, NULL, '', '', 1, NULL, NULL);
 
 
 #############################
@@ -94,7 +137,7 @@ SELECT "TABLE product";
 DROP TABLE IF EXISTS product;
 CREATE TABLE product(
     product_id          	INT(6)              DEFAULT NULL AUTO_INCREMENT,
-    pgroup_id           	INT(6)              REFERENCES product_group(mfg_id),
+    pgroup_id           	INT(6)              REFERENCES product_group(pgroup_id),
     product_name        	VARCHAR(100)        NOT NULL UNIQUE,
     taxfree             	ENUM('N','Y')       DEFAULT NULL,
 	user_id					INT(6)				NOT NULL REFERENCES user(user_id),
@@ -120,7 +163,58 @@ INSERT INTO product VALUES
     (15,  1,  	"Abocal 500 Tablet",                                        	"N",    1,	NULL, NULL),
     (16,  1,  	"Abtelmi-Amh Tablet",                                       	"N",    1,	NULL, NULL),
     (17,  1,  	"Abtorsmed 10 Tablet",                                       	"N",    1,	NULL, NULL),
-    (18,  1,  	"Abturbi Cream",                                             	"N",    1,	NULL, NULL);
+    (18,  1,  	"Abturbi Cream",                                             	"N",    1,	NULL, NULL),
+	#
+	(19,  2,    "Abatitor 250mg Tablet",                                        "N",    1,	NULL, NULL),
+	(20,  2,    "Acegesic 100 mg/325 mg Tablet",                                "N",    1,	NULL, NULL),
+	(21,  2,    "Acnex 10mg Capsule",                                           "N",    1,	NULL, NULL),
+	(22,  2,    "Acyclosure TH 8 Tablet",                                       "N",    1,	NULL, NULL),
+	(23,  2,    "Adfrar Injection",                                             "N",    1,	NULL, NULL),
+	(24,  2,    "Afoglip M 1000 Tablet ER",                                     "N",    1,	NULL, NULL),
+	(25,  2,    "Afoglip M 500 Tablet ER",                                      "N",    1,	NULL, NULL),
+	(26,  2,    "Agotine 25mg Tablet",                                          "N",    1,	NULL, NULL),
+	(27,  2,    "Ahaglow Advanced Skin Rejuvenating Face Wash",                 "N",    1,	NULL, NULL),
+	(28,  2,    "Ahaglow Advanced Skin Rejuvenating Face Wash",                 "N",    1,	NULL, NULL),
+	(29,  2,    "Ahaglow Advanced Skin Rejuvenating Face Wash",                 "N",    1,	NULL, NULL),
+	(30,  2,    "Ahaglow S Foaming Face Wash",                                  "N",    1,	NULL, NULL),
+	(31,  2,    "Ahaglow S Foaming Face Wash",                                  "N",    1,	NULL, NULL),
+	(32,  2,    "Allerfex 120mg Tablet",                                        "N",    1,	NULL, NULL),
+	(33,  2,    "Alprax 0.25 Tablet",                                           "N",    1,	NULL, NULL),
+	(34,  2,    "Alprax 0.5mg Tablet",                                          "N",    1,	NULL, NULL),
+	(35,  2,    "Alprax 1mg Tablet SR",                                         "N",    1,	NULL, NULL),
+	(36,  2,    "Alprax Plus Tablet SR",                                        "N",    1,	NULL, NULL),
+	(37,  2,    "Altipod 100mg Tablet DT",                                      "N",    1,	NULL, NULL),
+	(38,  2,    "Altipod CV 100mg/62.5mg Tablet DT",                            "N",    1,	NULL, NULL),
+	(39,  2,    "Amazeo 100 Tablet",                                            "N",    1,	NULL, NULL),
+	(40,  2,    "Amazeo 200 Tablet",                                            "N",    1,	NULL, NULL),
+	(41,  2,    "Amazeo OD 50mg Tablet",                                        "N",    1,	NULL, NULL),
+	(42,  2,    "Ambiz 10mg Tablet",                                            "N",    1,	NULL, NULL),
+	(43,  2,    "Amifru 40 Tablet",                                             "N",    1,	NULL, NULL),
+	(44,  2,    "Amlocor 10mg Tablet",                                          "N",    1,	NULL, NULL),
+	(45,  2,    "Amlocor 5mg Tablet",                                           "N",    1,	NULL, NULL),
+	(46,  2,    "Ampoxin LB Capsule",                                           "N",    1,	NULL, NULL),
+	(47,  2,    "Ampoxin-CV 200mg/28.5mg Suspension",                           "N",    1,	NULL, NULL),
+	(48,  2,    "Androz 25mg Tablet",                                           "N",    1,	NULL, NULL),
+	(49,  2,    "Androz 50mg Tablet",                                           "N",    1,	NULL, NULL),
+	(50,  2,    "Angiopril 25mg Tablet",                                        "N",    1,	NULL, NULL),
+	(51,  2,    "Anticox 4 Kit",                                             	"N",    1,	NULL, NULL),
+	(52,  2,    "Anticox II 450 mg/300 mg Tablet",                              "N",    1,	NULL, NULL),
+	(53,  2,    "Anticox Z 450 mg/300 mg/750 mg Tablet",                        "N",    1,	NULL, NULL),
+	(54,  2,    "Anxipar 0.25mg Tablet",                                        "N",    1,	NULL, NULL),
+	(55,  2,    "Arip MT 20 Tablet",                                            "N",    1,	NULL, NULL),
+	(56,  2,    "Arip MT 30 Tablet",                                            "N",    1,	NULL, NULL),
+	(57,  2,    "Arkamin H Tablet",                                             "N",    1,	NULL, NULL),
+	(58,  2,    "Aroff 200mg Tablet SR",                                        "N",    1,	NULL, NULL),
+	(59,  2,    "Asthafen Syrup",                                             	"N",    1,	NULL, NULL),
+	(60,  2,    "Asthafen Tablet",                                             	"N",    1,	NULL, NULL),
+	(61,  2,    "Asthesia Cream",                                             	"N",    1,	NULL, NULL),
+	(62,  2,    "Atlura 40 Tablet",                                             "N",    1,	NULL, NULL),
+	(63,  2,    "Atlura 80 Tablet",                                             "N",    1,	NULL, NULL),
+	(64,  2,    "Atmost 50mg Tablet",                                           "N",    1,	NULL, NULL),
+	(65,  2,    "Azibold 100mg/5ml Syrup",                                      "N",    1,	NULL, NULL),
+	(66,  2,    "Azisuper 100mg Suspension",                                    "N",    1,	NULL, NULL),
+	(67,  2,    "Azisuper 500mg Tablet",                                        "N",    1,	NULL, NULL),
+	(68,  2,    "Azona 20mg Capsule",                                           "N",    1,	NULL, NULL);
 
 
 #############################
@@ -201,7 +295,7 @@ CREATE TABLE stock(
     PRIMARY KEY(product_id)
 );
 INSERT INTO stock VALUES
-    (1,     "EZ0316",      "03/2024",      "02/2022",        59.00,       65.00,      75.00,      0,     1,	NULL, NULL);
+    (1,     "EZ0316",      "03/2024",      "02/2022",        59.00,       65.00,      75.00,      100,     1,	NULL, NULL);
 
 
 #############################
@@ -212,10 +306,12 @@ INSERT INTO stock VALUES
 SELECT "trn_header";
 DROP TABLE IF EXISTS trn_header;
 CREATE TABLE trn_header(
-	trn_no					INT(10)				NOT NULL AUTO_INCREMENT,
+	trn_slno				INT(10)				NOT NULL AUTO_INCREMENT,
 	trn_date				DATE				NOT NULL,
+	trn_no                  INT(10)             NOT NULL,
 	trn_type				INT(6)				NOT NULL REFERENCES trn_type(trn_type_id),
 	party_id				INT(6)				NOT NULL REFERENCES party(party_id),
+
 );
 
 
